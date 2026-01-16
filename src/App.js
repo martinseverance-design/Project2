7a8a3b60bad257e107c4181b14fccbb1f2f7d441
Martin Severance <martin.severance@centre.edu>
2026-01-16 12:20:51 -0500

    table data is started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect , useState } from "react";
 function App() {
+  const[grant, setGrant] = useState([{}])
+  useEffect(() => 
+  {
+    async function fetchGrant()
+    {
+      const url = '/NEH2020sGrant_Short.json'
+      console.log("hi")
+      const response = await fetch(url);
+      console.log(response.ok)
+      if (response.ok)
+         {
+            const result = await response.json();
+            console.log("to")
+            console.log(result.Grants.Grant)
+            setGrant(result.Grants.Grant)
+         }
+
+
+    }
+    
+    fetchGrant();
+    
+    },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <tbody>
+      {grant.map((grant, index) => (
+        <tr key = {grant["@AppNumber"]}>
+          <td>{grant.Institution}</td>
+          <td>
+            {grant.Institution}
+          </td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
   );
 }
 
